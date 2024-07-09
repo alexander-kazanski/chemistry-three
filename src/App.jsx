35494275f064ae1d0ad3b08d8e9a2973b15a4443
i.go.by.ak <i.write.code.and.lift.weights@gmail.com>
2024-07-09 13:27:21 -0400

    removing unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Canvas } from "@react-three/fiber";
-import { useState, Suspense } from "react";
-import { Environment } from "@react-three/drei";
-import { Model } from "./Model";
 import Scene from "./Scene";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div style={{ width: '100vw', height: '100vh'}}>
       <Scene />
